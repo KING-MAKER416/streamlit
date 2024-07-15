48c55563fb9ab87f9914600eb6b9f80b7613a9b9
KING-MAKER416 <jwbdk56@gmail.com>
2024-07-15 18:22:59 +0900

    Update 04_streamlit_openai.py

--- 04_streamlit_openai.py
+++ 04_streamlit_openai.py
@@ -12,8 +12,8 @@ with st.sidebar:
     st.markdown(iframe_html, unsafe_allow_html=True)
     st.info("이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.")
     
-    openai_api_key = st.text_input("OpenAI API KEY", type="password")
-    client = OpenAI(api_key="sk-proj-xpxpJFnNxGzxPExRjjmtT3BlbkFJMIBwJOGcBkYZJgkMlRRc")
+    openai_api_key = "sk-proj-xpxpJFnNxGzxPExRjjmtT3BlbkFJMIBwJOGcBkYZJgkMlRRc"
+    client = OpenAI(api_key=openai_api_key)
     thread_id = st.text_input("Thread ID", value=thread_id)
 
     thread_make_btn = st.button("Create a new thread")
